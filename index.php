9479bc877b713abfb428788147261e92638ae02c
Ivan Loeza <ijlq_loeza@hotmail.com>
2021-06-13 17:15:20 -0500

    changes not commited from last deploy

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php
+session_start();
 require 'Modelos/db.php';  //LarryDrive#1
 require 'Controladores/controlador.php';
-$controller = 'login';
+require_once 'Vistas/partials/layout_head.php';
+$controller_request = isset($_REQUEST['c']) ? $_REQUEST['c'] : 'login';
+
 
 // Todo esta lógica hara el papel de un FrontController
-if(!isset($_REQUEST['c'])) {
-    require_once "Controladores/{$controller}_controller.php";
-    $controller = ucwords($controller) . 'Controller';
-    $controller = new $controller;
-    $controller->Index();  
-} else {
+if($controller_request !== 'login') {
     // Obtenemos el controlador que queremos cargar
-    $controller = strtolower($_REQUEST['c']);
     $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
-
-    // Instanciamos el controlador
-    require_once "Controladores/{$controller}_controller.php";
-    $controller = ucwords($controller) . 'Controller';
-    $controller = new $controller;
-
     // Llama la accion
-    call_user_func( array( $controller, $accion ) );
-}
 
+	if((isset($_SESSION['id']) && $_SESSION['id'] != '') || $controller_request == 'signin' || $accion == 'login' || $accion == 'crear'){
+
+		// Instanciamos el controlador
+		require_once "Controladores/{$controller_request}_controller.php";
+		$controller_name = ucwords($controller_request) . 'Controller';
+		$controller = new $controller_name;
+
+    	call_user_func( array( $controller, $accion ) );
+
+	} else {
+		Controlador::redirect("Necesitas loggearte para ver esta pagina");
+	}
+
+} else {
+	require_once "Controladores/login_controller.php";
+	$controller = new LoginController;
+	$controller->Index();  
+}
+require_once 'Vistas/partials/layout_footer.html';
